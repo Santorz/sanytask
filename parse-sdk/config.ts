82b94c45d783aca3fea8c898cbef8b0b700b29fa
Santorz <saintbrisbe@gmail.com>
2022-06-29 14:12:42 +0100

    Added environmental variables

--- parse-sdk/config.ts
+++ parse-sdk/config.ts
@@ -1,4 +1,6 @@
 // Your Parse initialization configuration goes here
-export const PARSE_APPLICATION_ID = "ByzKNvq3mazCdB28zMXJosYmokhScEPiRXOqpsQl";
-export const PARSE_HOST_URL = "https://parseapi.back4app.com/";
-export const PARSE_JAVASCRIPT_KEY = "CswvoblAMJQSqBvVrPulyPuIMDtm7DoLQK2h968q";
+export const PARSE_APPLICATION_ID =
+  process.env.NEXT_PUBLIC_PARSE_APPLICATION_ID;
+export const PARSE_HOST_URL = process.env.NEXT_PUBLIC_PARSE_HOST_URL;
+export const PARSE_JAVASCRIPT_KEY =
+  process.env.NEXT_PUBLIC_PARSE_JAVASCRIPT_KEY;
